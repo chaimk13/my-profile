26673f90188709f473acdb83c5729764fc9ecb82
chaimk13 <shayackohn@gmail.com>
2017-05-09 18:10:49 -0400

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,8 @@
+var password =  "hello";
+password = prompt ("enter your password");
+
+if (password = "hello" ) {
+     alert ("password is correct");
+} else {
+     alert ("wrong password, try again");
+}
